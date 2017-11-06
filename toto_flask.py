7b1631d817206dffcd5a6139762cd36f492c4472
吴启德 <khee_teck_goh@apple.com>
2017-11-07 00:17:06 +0800

    first checkin

---
+++ toto_flask.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pythonw
+
+#################################
+# TOTO Analyzer for Alexa 	#
+# Goh Khee Teck			#
+# pydot4 clas			#
+#################################
+
+from toto_analyzer import get_past_results,get_last_drawn_num_rows
+from flask import Flask
+from flask_ask import Ask,statement,question
+
+app = Flask(__name__)
+ask = Ask(app, "/toto_analyzer")
+
+@app.route("/")
+def hompage():
+	return 	"""
+		This is a tool developed as part of the 'Python for Data, Ops and Things' course.\n
+		It collect and analyse data from Singapore pool.
+		"""
+@ask.launch
+def start_skill():
+	welcome_message = "Hello master, do you like to know the latest toto results ?"
+	return question(welcome_message)
+
+@ask.intent("YesIntent")
+def get_last_draw():
+	url = "http://www.singaporepools.com.sg/en/product/Pages/toto_results.aspx"
+	dict_of_results = get_past_results(url,False)
+	_,list_of_draws = get_last_drawn_num_rows(dict_of_results,1)
+	draw_result = " ".join(num for num in list_of_draws)
+
+	return statement(draw_result)
+
+@ask.intent("NoIntent")
+def no_intent():
+	bye_text = "I am not sure why you ask me to run then, but okie goodbye"
+	return statement(bye_text)
+
+if __name__ == "__main__":
+	app.run(debug=True)
